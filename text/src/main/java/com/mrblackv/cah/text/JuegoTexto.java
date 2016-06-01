cff23d3414343ac2556ea5aeb2a643d9f2938086
v.chico <v.chico@IT013.indizen.com.es>
2016-06-01 17:55:37 +0200

    Añadido método para obtener respuestas de los jugadores

--- text/src/main/java/com/mrblackv/cah/text/JuegoTexto.java
+++ text/src/main/java/com/mrblackv/cah/text/JuegoTexto.java
@@ -7,8 +7,8 @@ package com.mrblackv.cah.text;
 
 import com.mrblackv.cah.obj.Carta;
 import com.mrblackv.cah.obj.Juego;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
+import com.mrblackv.cah.obj.Jugador;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -21,12 +21,14 @@ public class JuegoTexto {
     private Juego juego;
     private Carta pregunta;
     private Scanner scan;
+    private int pick;
     
     public JuegoTexto() {
         scan = new Scanner(System.in);
         System.out.println("Cartas contra los Homínidos");
         System.out.println("___________________________\n");
         configuraJuego();
+        juego();
     }
     
     private void configuraJuego() {
@@ -90,4 +92,68 @@ public class JuegoTexto {
         } while (!correcto);
     }
     
+    private void juego() {
+        while (juego.nuevoTurno()) {
+            pregunta = juego.getPreguntaTurno();
+            System.out.println("Turno: "+juego.getTurnoActual()+"\n");
+            System.out.println("Jugador: " + (juego.getZar()+1) + " (Zar)");
+            System.out.println(".......................\n");
+            pick = 1;
+            if (pregunta.getExtra() != null) {
+                Map<String, Integer> extra = pregunta.getExtraMap();
+                if (extra.get("DRAW") != null) {
+                    juego.repartir(extra.get("DRAW"));
+                }
+                if (extra.get("PICK") != null) {
+                    pick = extra.get("PICK");
+                }
+            }
+            for (Jugador j : juego.getJugadores()) {
+                if (j.getId() != juego.getZar()) {
+                    getRespuestas(j);
+                }
+            }
+            System.out.println(pregunta.getTexto());
+            System.out.println("______________________________\n");
+            for (Carta c : juego.getRespuestasTurno()) {
+                System.out.println("Jugador: "+c.getUserID());
+                System.out.println(c.getTexto()+"\n");
+            }
+        }
+    }
+    
+    private void getRespuestas(Jugador j) {
+        int elegidas = 0;
+        boolean correcto;
+        do {
+            System.out.println("Jugador: " + (j.getId()+1));
+            System.out.println("Elige una carta: \n");
+            System.out.println(pregunta.getTexto());
+            System.out.println(".......................\n");
+            for (int i = 0; i<j.getMano().size(); i++) {
+                System.out.println("Carta "+(i+1)+":");
+                System.out.println(j.getMano().get(i).getTexto()+"\n");
+            }
+            do {
+                correcto = true;
+                String elegida;
+                try {
+                    elegida = scan.nextLine();
+                    if (Integer.parseInt(elegida) < 1 || Integer.parseInt(elegida) > j.getMano().size()){
+                        System.err.println("No se ha introducido un número correcto");
+                        correcto = false;
+                    } else {
+                        Carta c = j.getMano().get(Integer.parseInt(elegida)-1);
+                        c.setUserID(j.getId()+1);
+                        j.getMano().remove(Integer.parseInt(elegida)-1);
+                        juego.addRespuesta(c);
+                        elegidas++;
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Se debe introducir un número");
+                    correcto = false;
+                }
+            } while (!correcto);
+        } while (elegidas < pick);
+    }
 }
